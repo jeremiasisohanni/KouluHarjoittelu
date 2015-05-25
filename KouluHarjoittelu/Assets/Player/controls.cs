c45eb2e2fa731bc4fe0ce7417fe55d81c69b5671
jeremias <jeremias.isohanni@gmail.com>
2015-05-25 15:52:38 +0300

    Säätöä lähinnä textuureihin ja maailmaan.

--- KouluHarjoittelu/Assets/Player/controls.cs
+++ KouluHarjoittelu/Assets/Player/controls.cs
@@ -9,7 +9,7 @@ public class controls : MonoBehaviour {
 	
 	private Vector3 moveDirection = Vector3.zero;
 
-	void Update () {
+	void FixedUpdate () {
 		CharacterController controller = GetComponent<CharacterController> ();
 		if (controller.isGrounded) {
 			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
