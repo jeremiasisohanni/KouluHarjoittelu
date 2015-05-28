4783fc64c840983c132a0d4c60f8772edce0671e
jeremias <jeremias.isohanni@gmail.com>
2015-05-28 13:07:37 +0300

    28.5.2015
    AI säätöä.

--- KouluHarjoittelu/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
+++ KouluHarjoittelu/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -10,6 +10,10 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         public NavMeshAgent agent { get; private set; } // the navmesh agent required for the path finding
         public ThirdPersonCharacter character { get; private set; } // the character we are controlling
         public Transform target; // target to aim for
+		private Vector3 currentPos;
+		private bool sight = false;
+		private float distance = 0.0f;
+
 
         // Use this for initialization
         private void Start()
@@ -17,29 +21,42 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             // get the components on the object we need ( should not be null due to require component so no need to check )
             agent = GetComponentInChildren<NavMeshAgent>();
             character = GetComponent<ThirdPersonCharacter>();
+			currentPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
 
 	        agent.updateRotation = false;
-	        agent.updatePosition = true;
+	        agent.updatePosition = true;;
         }
 
+	
+		private void OnTriggerEnter (Collider other) {
+			if(other.tag == "Player")
+				sight = true;
+
+		}
 
         // Update is called once per frame
         private void Update()
         {
-            if (target != null)
+			distance = Vector3.Distance(target.position, transform.position);
+			print ("Distance: " + distance);
+
+		if (target != null && sight == true)
             {
                 agent.SetDestination(target.position);
-
-				
-				
                 // use the values to move the character
                 character.Move(agent.desiredVelocity, false, false);
+
             }
-            else
-            {
-                // We still need to call the character's move function, but we send zeroed input as the move param.
-                character.Move(Vector3.zero, false, false);
-            }
+		else if (distance > 10) {
+				sight = false;
+				agent.SetDestination(currentPos);
+				character.Move(agent.desiredVelocity,false, false);
+			}
+			else
+			{
+				// We still need to call the character's move function, but we send zeroed input as the move param.
+				character.Move(Vector3.zero, false, false);
+			}
 
         }
 
