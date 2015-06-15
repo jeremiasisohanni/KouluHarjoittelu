a7663541f6bc34d9a43fa3332e5fb3599f8a5fa2
jeremias <jeremias.isohanni@gmail.com>
2015-06-15 15:55:11 +0300

    dynamic wall 2

---
+++ KouluHarjoittelu/Assets/wallCreation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wallCreation : MonoBehaviour {
+
+	public GameObject start;
+	public GameObject end;
+
+	// Use this for initialization
+	void Start () {
+	            
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+}
+	Vector3 getWorldPoint() {
+		Vector3 y = new Vector3 (0, 0, 0);
+		return y;
+	}
+}
