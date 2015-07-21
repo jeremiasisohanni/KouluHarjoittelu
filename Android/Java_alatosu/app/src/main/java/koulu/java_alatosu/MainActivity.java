e632f37ca603fbcbb4bc4661b34442c17805d0a8
jeremias <jeremias.isohanni@gmail.com>
2015-07-21 15:48:12 +0300

    ti 21.7.2015
    android

---
+++ Android/Java_alatosu/app/src/main/java/koulu/java_alatosu/MainActivity.java
@@ -0,0 +1,64 @@
+package koulu.java_alatosu;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.graphics.Color;
+
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Layout
+        RelativeLayout relLayot = new RelativeLayout(this);
+        relLayot.setBackgroundColor(Color.GREEN);
+
+        RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        //Button
+        Button redButton = new Button(this);
+        redButton.setText("Click me Silly");
+        redButton.setBackgroundColor(Color.RED);
+
+        //piirrä nappula
+        relLayot.addView(redButton, buttonDetails);
+
+        setContentView(relLayot);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
