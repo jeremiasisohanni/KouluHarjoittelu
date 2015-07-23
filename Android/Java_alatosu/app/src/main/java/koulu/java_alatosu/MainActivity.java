d2b7acea6239c8058993bc0c5e288480e5c18632
jeremias <jeremias.isohanni@gmail.com>
2015-07-23 15:45:52 +0300

    Android
    23.7.2015

--- Android/Java_alatosu/app/src/main/java/koulu/java_alatosu/MainActivity.java
+++ Android/Java_alatosu/app/src/main/java/koulu/java_alatosu/MainActivity.java
@@ -7,8 +7,9 @@ import android.view.MenuItem;
 import android.widget.RelativeLayout;
 import android.widget.Button;
 import android.graphics.Color;
-
-
+import android.widget.EditText;
+import android.content.res.Resources;
+import android.util.TypedValue;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -21,21 +22,45 @@ public class MainActivity extends ActionBarActivity {
         RelativeLayout relLayot = new RelativeLayout(this);
         relLayot.setBackgroundColor(Color.GREEN);
 
+        //Button
+        Button redButton = new Button(this);
+        redButton.setText("Log In");
+        redButton.setBackgroundColor(Color.RED);
+
+        //Username input
+        EditText username = new EditText(this);
+
+        redButton.setId(1);
+        username.setId(2);
+
         RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
                 RelativeLayout.LayoutParams.WRAP_CONTENT,
                 RelativeLayout.LayoutParams.WRAP_CONTENT
         );
 
+        RelativeLayout.LayoutParams usernameDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        //Give rules to position widgets
+
+        usernameDetails.addRule(RelativeLayout.ABOVE, redButton.getId());
+        usernameDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        usernameDetails.setMargins(0, 0, 0, 50);
+
         buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
         buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
 
-        //Button
-        Button redButton = new Button(this);
-        redButton.setText("Click me Silly");
-        redButton.setBackgroundColor(Color.RED);
+
+        Resources r = getResources();
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,200,r.getDisplayMetrics());
+
+        username.setWidth(px);
 
         //piirrä nappula
         relLayot.addView(redButton, buttonDetails);
+        relLayot.addView(username, usernameDetails);
 
         setContentView(relLayot);
     }
